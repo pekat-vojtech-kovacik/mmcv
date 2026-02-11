2c4bbd2d8ca768f710d3ccc45e6af4fb7798f3f1
vojtech kovacik <vojtech.kovacik@pekatvision.com>
2026-02-11 13:27:39 +0100

    build: :heavy_minus_sign: Remove pkg_resources dependency (python >=3.12 does not support it)

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ import glob
 import os
 import platform
 import re
-from pkg_resources import DistributionNotFound, get_distribution, parse_version
+from importlib.metadata import version, PackageNotFoundError
+from packaging.version import parse as parse_version
 from setuptools import find_packages, setup
 
 EXT_TYPE = ''
@@ -33,8 +34,8 @@ def choose_requirement(primary, secondary):
     return secondary."""
     try:
         name = re.split(r'[!<>=]', primary)[0]
-        get_distribution(name)
-    except DistributionNotFound:
+        version(name)
+    except PackageNotFoundError:
         return secondary
 
     return str(primary)
